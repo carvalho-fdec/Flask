69035c48e302d656c0009ed3ee44ac38fd52de03
Fernando <carvalho.fdec@gmail.com>
2022-04-09 20:10:40 -0300

    caminhos
    uso de caminhos e template em html

--- hello.py
+++ hello.py
@@ -1,11 +1,28 @@
-from flask import Flask
+from flask import Flask, flash, redirect, render_template, request, session, url_for, abort
+
 
 app = Flask(__name__)
+
 @app.route('/')
-def hello_world():
-    return 'Hello, World!'
+def index():
+    return 'index'
+
+@app.route('/hello')
+def hello():
+    return 'hello world'
+
+@app.route('/user/<name>')
+def user(name):
+    return f'<h1>Hello, {name} </h1>'
+
+@app.route('/hello/<name>')
+def hello_name(name):
+    return render_template('template1.html', name=name.upper())
+
+
 
 if "__main__" == __name__:
     app.run(debug=True) 
+    #app.run(host='0.0.0.0', port=80)
 
-    
+
